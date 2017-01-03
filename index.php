7279a5156e9912e87bbca27c635a60ceaae8ea43
Alex Toff <admin@cobaltgrid.com>
2017-01-03 19:31:02 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$requiredInput = array("depICAO", "destICAO");
+
+$reservedCodes = array("1200", "7500", "7600", "7000", "7777");
+
+foreach($requiredInput as $required){
+  if(!isset($_GET[$required])){
+    die("A required input was not supplied! (" . $required . ")");
+  }else{
+    $$required = $_GET[$required];
+  }
+}
+
+
+
+$depCountryCode = substr($depICAO, 0, 2);
+$destCountryCode = substr($destICAO, 0, 2);
+
+
+ ?>
