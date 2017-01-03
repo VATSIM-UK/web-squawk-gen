376ba4498aa01de28e07d88bcb01cc30fa0eb284
Alex Toff <admin@cobaltgrid.com>
2017-01-03 20:48:43 +0000

    Creation of functions page. Functions to parse the range, and generate a squawk from it

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?php
+
+function parseSquawkRange($input){
+  $split = explode("-", $input);
+  return $split;
+
+}
+
+function outputSquawk($range){
+    global $reservedCodes;
+
+    // Pick a code at random from the range
+    $number = rand($range[0], $range[1]);
+    $output = 0;
+
+    // Check if the code it has given is 3 digits. If so, append a 0 at the start
+    if($number < 1000){
+      $output = "0" . $number;
+    }else{
+      $output = $number;
+    }
+
+    // Check if it is reserved
+    if(array_search($output, $reservedCodes)){
+      // Get another!
+      outputSquawk($range);
+    }else{
+      // Output the Squawk Code
+      echo $output;
+      exit();
+    }
+}
+
+
+
+
+
+ ?>
