4599b3b55866b303242dfb8264eef13c5aaa3baf
Alex Toff <admin@cobaltgrid.com>
2017-01-03 20:50:08 +0000

    Add more reserved codes and configuartion variables

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 include("functions.php");
 
-$requiredInput = array("depICAO", "destICAO");
+$requiredInput = array("destICAO");
 
-$reservedCodes = array("1200", "7500", "7600", "7000", "7777");
+$reservedCodes = array("1200", "7500", "7600", "7000", "7777", "1000", "2000", "7007", "0024","0033","0450","1177","4520","7001","7002","7003","7004","7005","7006","7010","7401", "0002","7776","0010","0011","0012","0013","0440","2620","2677","3660","4517","4572","5077","6170","7045","7366");
+
+$tableName = "squawks";
+
+$db = new SQLite3('database.sqlite3');
+
 
 foreach($requiredInput as $required){
   if(!isset($_GET[$required])){
