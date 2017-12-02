5058c147e905f933dac07cd1664fe545a7e6b140
Alex Toff <admin@cobaltgrid.com>
2017-12-02 15:25:35 +0000

    Fix issue with squawk codes begining with a zero

--- src/api/functions.php
+++ src/api/functions.php
@@ -9,7 +9,7 @@ class AllocationDB extends SQLite3
         // Check for setup
         $this->exec('CREATE TABLE IF NOT EXISTS `recent_allocations` (
                         	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-                        	`squawk`	INTEGER NOT NULL,
+                        	`squawk`	TEXT NOT NULL,
                         	`allocated_at`	TEXT NOT NULL
                         )');
     }
@@ -23,7 +23,14 @@ function parseSquawkRange($input)
 
 function findSquawk($range, $final = false)
 {
-    $result = outputSquawk($range, $final);
+    $shouldSearchRange = true;
+    while($shouldSearchRange){
+      $result = outputSquawk($range, $final);
+      if($result != 2){
+        $shouldSearchRange = false;
+      }
+    }
+
     return $result;
 }
 
@@ -33,25 +40,28 @@ function initAllocationDB()
 }
 
 $timesTried = 0;
+$squawkFound = false;
 function outputSquawk($range, $final = false)
 {
-    global $reservedCodes;
-    global $allocationDB;
-    global $timesTried;
+    // Response of false = Couldn't find a squawk in range
+    // Response of 2 (int) = Keep searching, random squawk generated not suitible
+    // Response of true (i.e a squawk) = Squawk found (and allocated)
+    global $reservedCodes, $allocationDB, $timesTried, $squawkFound;
     $timesTried++;
     $bypassAllocatedCheck = false;
+    $rangeOfRange = abs($range[1]-$range[0]) + 1;
 
-    if ($timesTried >= 40 && $final) {
+    if (($timesTried >= 40 || $timesTried > $rangeOfRange) && $final) {
         // Just generate a random one...
         $bypassAllocatedCheck = true;
         $number = rand(0, 7777);
-    } elseif ($timesTried >= 40) {
+    } elseif ($timesTried >= 40 || $timesTried > $rangeOfRange) {
         return false;
     } else {
         // Pick a code at random from the range
         $number = rand($range[0], $range[1]);
     }
-
+    
     $output = 0;
 
     // Check if the code it has given is 3 digits. If so, append a 0 at the start
@@ -66,21 +76,15 @@ function outputSquawk($range, $final = false)
     foreach ($splitCode as $digit) {
         if ($digit > 7) {
             // Get another!
-            outputSquawk($range);
+            return 2;
         }
-    }
-
-    // For good measure, also check that it is 4 digits long
-    if (strlen($number) != 4) {
-        // Get another!
-        outputSquawk($range);
     }
 
 
     // Check if it is reserved
     if (array_search($output, $reservedCodes)) {
         // Get another!
-        outputSquawk($range);
+        return 2;
     }
 
     if ($bypassAllocatedCheck) {
@@ -95,16 +99,16 @@ function outputSquawk($range, $final = false)
     $res = $allocationDB->query("SELECT allocated_at FROM ".$allocationTableName." WHERE squawk='".$output."'");
     if ($arr = $res->fetchArray(SQLITE3_NUM)) {
         if (timestampExpired($arr[0])) {
-            $allocationDB->exec("UPDATE ".$allocationTableName." SET allocated_at='".date('Y-m-d H:i')."' WHERE squawk = ".$output);
+            $allocationDB->exec("UPDATE ".$allocationTableName." SET allocated_at='".date('Y-m-d H:i')."' WHERE squawk = '".$output."'");
             $allocationDB->close();
-            return $output;
+            return 2;
         }
         // Get another!
         $allocationDB->close();
         outputSquawk($range);
     } else {
         // Output the Squawk Code
-        $allocationDB->exec("INSERT INTO ".$allocationTableName." (squawk, allocated_at) VALUES (".$output.",'".date('Y-m-d H:i')."')");
+        $allocationDB->exec("INSERT INTO ".$allocationTableName." (squawk, allocated_at) VALUES ('".$output."','".date('Y-m-d H:i')."')");
         $allocationDB->close();
         return $output;
     }
@@ -115,14 +119,14 @@ function runICAOChecks()
     global $destICAO;
     global $depICAO;
     global $db;
-
-    if (strlen($destICAO) != 4 || !ctype_upper($destICAO)) {
+    $destICAO = strtoupper($destICAO);
+    $depICAO = strtoupper($depICAO);
+    if (strlen($destICAO) != 4) {
         die();
     }
-    if ($depICAO != "" && (strlen($destICAO) != 4 || !ctype_upper($destICAO))) {
+    if ($depICAO != "" && strlen($destICAO) != 4) {
         die();
     }
-
     $GLOBALS['destICAO'] = $db::escapeString(htmlspecialchars($destICAO));
     $GLOBALS['depICAO'] = $db::escapeString(htmlspecialchars($depICAO));
 }
